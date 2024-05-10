74378504cfca30f107bd7e7ed47dcb28e4790f46
Jordan Sinek <jordan.sinek@vincit.fi>
2024-05-10 15:13:48 -0600

    Increase back navigation cooldown

--- navigation/nav.ts
+++ navigation/nav.ts
@@ -49,7 +49,7 @@ export const nav: SkeletonNav = {
 
     setTimeout(() => {
       tmpPreventGoBack = false;
-    }, options?.cooldownTime || 200);
+    }, options?.cooldownTime || 2000);
   },
   canGoBack: () => navRef.isReady() && navRef.canGoBack(),
 };
