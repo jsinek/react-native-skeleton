ae5145a2c35b1a81ad999ca28dca1a614a9d4464
Jordan Sinek <jordan.sinek@pawport.com>
2024-07-31 09:04:20 -0600

    Fix UI element positioning

--- components/UI/UIElement.tsx
+++ components/UI/UIElement.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import {Dimensions, LayoutChangeEvent, StyleSheet, View, ViewProps} from 'react-native';
-import {UIContext, useUIElements} from '../../context/UI';
-import { getScreenConfig } from '../../navigation/screen';
-import { UIPosition } from '../../types/ui';
-export const screenDimensions = Dimensions.get('screen');
+import React from 'react'
+import {
+  Dimensions,
+  LayoutChangeEvent,
+  StyleSheet,
+  View,
+  ViewProps,
+} from 'react-native'
+import { UIContext, useUIElements } from '../../context/UI'
+import { getScreenConfig } from '../../navigation/screen'
+import { UIPosition } from '../../types/ui'
+export const screenDimensions = Dimensions.get('screen')
 
 export interface UIElementProps extends ViewProps {
-  edge: UIPosition;
+  edge: UIPosition
 }
 
 export const UIElement = ({ edge, ...rest }: UIElementProps) => {
   const onLayout = (e: LayoutChangeEvent) => {
-    const screen = getScreenConfig();
+    const screen = getScreenConfig()
     if (screen?.name) {
-      UIContext.layout[edge].height.setValue(e.nativeEvent.layout.height);
-      UIContext.layout[edge].width.setValue(e.nativeEvent.layout.width);
+      UIContext.layout[edge].height.setValue(e.nativeEvent.layout.height)
+      UIContext.layout[edge].width.setValue(e.nativeEvent.layout.width)
     }
-    rest.onLayout?.(e);
+    rest.onLayout?.(e)
   }
 
-  return <View {...rest} style={[styles[edge], rest.style]} onLayout={onLayout}>{useUIElements()[edge]}</View>;
-};
+  return (
+    <View {...rest} style={[styles[edge], rest.style]} onLayout={onLayout}>
+      {useUIElements()[edge]}
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   top: {
@@ -31,20 +41,20 @@ const styles = StyleSheet.create({
   },
   left: {
     position: 'absolute',
-    bottom: 0,
+    bottom: -screenDimensions.height,
     left: 0,
     top: 0,
   },
   right: {
     position: 'absolute',
-    bottom: 0,
+    bottom: -screenDimensions.height,
     top: 0,
     right: 0,
   },
   bottom: {
     position: 'absolute',
-    bottom: 0,
+    bottom: -screenDimensions.height,
     left: 0,
     right: 0,
-  }
-});
+  },
+})
